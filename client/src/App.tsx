26dac83d300338dfbf1059ca1a85900d9530d0ef
crocodama <86365509+crocodama@users.noreply.github.com>
2025-01-28 15:23:36 +0200

    started on home page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,4 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import Card from './view/components/card'
+
 import Router from './Router'
 
 
--- client/src/view/pages/home/Home.tsx
+++ client/src/view/pages/home/Home.tsx
@@ -1,7 +1,11 @@
-import React from 'react'
-
+import './home.scss'
 export default function Home() {
   return (
-    <div>Home</div>
+    <>
+    <div className="logoDiv"><img src="/simplemmo-dragon.png" alt="aaaa"/></div>
+    <div className="content">
+    <h1>Welcome to Simple-MMO Clone!</h1>
+    </div>
+    </>
   )
 }
